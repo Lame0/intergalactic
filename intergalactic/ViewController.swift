129f90d84476d7a1e6534db279c2e9915df4bf0c
Damian <damianna070@gmail.com>
2022-10-31 10:30:18 -0600

    added app icon and icon

--- intergalactic/ViewController.swift
+++ intergalactic/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func assignbackground(){
+        let background = UIImage(named: "pop")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        
+        
+        
+    }
 }
-
